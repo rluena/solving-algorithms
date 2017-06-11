64d2583d0df6072764a8074eadbbf33cd8a50ceb
Rabii Luena <luenarabii@gmail.com>
2017-06-11 09:31:22 +0300

    Completing factorization

--- app/basic.js
+++ app/basic.js
@@ -1,5 +1,6 @@
 /**
  * @param {String} str
+ * @return {String}
  */
 module.exports.reverseString = function (str) {
     var newStr = '';
@@ -7,4 +8,24 @@ module.exports.reverseString = function (str) {
         newStr += str[i];
     }
     return newStr;
+}
+
+/**
+ * @param {Number} num
+ * @return {Number}
+ */
+module.exports.factorize = function( num ){
+
+    var total = 1;
+
+    if (typeof(num) !== 'number') return false;
+
+    if (num === 0) {
+        total = 1;
+    } else {
+         for(var i = 1; i <= num; i++){
+             total *= i;
+         }
+    }
+    return total;
 }
--- test/basic.spec.js
+++ test/basic.spec.js
@@ -1,17 +1,33 @@
 const mocha = require('mocha');
 const expect = require('chai').expect;
 // const should = require('chai').should;
-const basic = require('../app/basic');
+const reverseString = require('../app/basic').reverseString;
+const factorize = require('../app/basic').factorize;
 
 describe('Basic Algorithms', () => {
-
+   
     describe('#reverseString', () => {
         it('should return a string', () => {
-            expect(basic.reverseString('Hello!')).to.be.a('string');
+            expect(reverseString('Hello!')).to.be.a('string');
         });
 
         it('should return a !olleH', () => {
-            expect(basic.reverseString('Hello!')).to.be.equal('!olleH');
+            expect(reverseString('Hello!')).to.be.equal('!olleH');
         });
-    })
-})
+    });
+
+   
+    describe('#factorize', () => {
+        it('should be a number', () => {
+            expect(factorize(5)).to.be.a('number');
+            expect(factorize('some string')).to.be.equal(false);
+        });
+
+        it('should return 1, 120, 3628800 and 2432902008176640000', () => {
+            expect(factorize(0)).to.be.equal(1);
+            expect(factorize(5)).to.be.equal(120);
+            expect(factorize(10)).to.be.equal(3628800);
+            expect(factorize(20)).to.be.equal(2432902008176640000);
+        });
+    });
+});

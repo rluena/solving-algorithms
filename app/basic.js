e5872c6eb892fa6855c0f4b4eb2cdb01ef8a88a0
Rabii Luena <luenarabii@gmail.com>
2017-06-29 22:31:20 +0300

    Solving of phase one of basic algorithms

--- app/basic.js
+++ app/basic.js
@@ -1,31 +1,186 @@
 /**
+ * Reverse the provided string.
+ * 
  * @param {String} str
  * @return {String}
  */
-module.exports.reverseString = function (str) {
-    var newStr = '';
-    for (var i = str.length - 1; i >= 0; i--) {
-        newStr += str[i];
-    }
-    return newStr;
+module.exports.reverseString = (str) => {
+	var newStr = '';
+	for (var i = str.length - 1; i >= 0; i--) {
+		newStr += str[i];
+	}
+	return newStr;
 }
 
 /**
+ * Return the factorial of the provided integer.
+ * 
  * @param {Number} num
  * @return {Number}
  */
-module.exports.factorize = function( num ){
+module.exports.factorize = (num) => {
 
-    var total = 1;
+	var total = 1;
 
-    if (typeof(num) !== 'number') return false;
+	if (typeof (num) !== 'number') return false;
 
-    if (num === 0) {
-        total = 1;
-    } else {
-         for(var i = 1; i <= num; i++){
-             total *= i;
-         }
-    }
-    return total;
+	if (num === 0) {
+		total = 1;
+	} else {
+		for (var i = 1; i <= num; i++) {
+			total *= i;
+		}
+	}
+	return total;
 }
+
+/**
+ * Return true if the given string is a palindrome. Otherwise, return false.
+ * 
+ * Removed all non-alphanumeric characters (punctuation, spaces and symbols)
+ * 
+ * @param {String} str
+ * @return {Boolean}
+ */
+/*module.exports.palindrome = (str) => {
+
+	// Removing white spaces and non-alphanumeric characters
+	var str = str.replace(/[\s\W_]+/g, '');
+	var newStr = str.split(' ').reverse().join();
+
+	return str === newStr ? true : false;
+}*/
+
+/**
+ * Find the longest word in a string
+ */
+module.exports.findLongestWord = (str) => {
+	var arr = str.split(' ');
+	var len = 0;
+
+	// Options number one
+	var longest = arr.reduce((len, next) => {
+		if (next.length > len) {
+			len = next.length;
+		}
+		return len;
+	}, 0);
+
+	// Options numbe two
+	// for (var i = 0; i < arr.length; i++) {
+	//     var word = arr[i];
+	//     len = word.length > len ? word.length : len;        
+	// }
+	return longest;
+}
+
+/**
+ * Capitalize each word from a string
+ */
+module.exports.titleCase = (str) => {
+	var arr = str.split(' ');
+	var newStr = arr.map((next) => {
+		var newArr = next.toLowerCase().split('');
+		var wordCased = '';
+		for (var i = 0; i < newArr.length; i++) {
+			if (i === 0) {
+				wordCased += newArr[i].toUpperCase();
+			} else {
+				wordCased += newArr[i];
+			}
+		}
+		return wordCased;
+	}).join(' ');
+
+	return newStr;
+}
+
+/**
+ * Find a largest number from four members of array
+ */
+module.exports.largestOfFour = (arr) => {
+	var newArr = [];
+	for (var i = 0; i < arr.length; i++) {
+		var numberArr = arr[i];		
+		var num = numberArr.reduce((largest, next) => {
+			largest = next > largest ? next : largest;
+			return largest;
+		}, 0);
+		newArr.push(num);
+	}
+
+	return newArr;
+}
+
+/**
+ * Confirm endings
+ */
+module.exports.confirmEnding = (word, str) => {
+	var count = str.length;
+	return word.substr(-count) === str ? true : false; 
+}
+
+/**
+ * Repeat string number of times
+ */
+module.exports.repeatStringNumTimes = (str, times) => {
+	var newStr = '';
+	for (var i = 0; i < times; i++) {
+		newStr += str;		
+	}
+	newStr = times < 0 ? '' : newStr;
+	return newStr;
+}
+
+/**
+ * Trancating string
+ */
+module.exports.truncateString = (str, len) => {
+	var newStr = '';
+	if (len >= str.length) {
+		newStr = str
+	} else {
+		len = len > 3 ? len - 3 : len;
+		newStr = str.slice(0, len) + '...';
+	}
+
+	return newStr;
+}
+
+/*var chunkArrayInGroups = (arr, len) => {
+	newArr = [];
+	return arr.slice(0, 2);
+}*/
+
+/*var slasher = (arr, len) => {
+
+	newArr = [];
+	// return arr.splice(0, 2);
+	return arr.slice(0, 2);
+}*/
+
+/**
+ * Return true if the string in the first element of 
+ * the array contains all of the letters of the string in the second element of the array.
+ */
+module.exports.mutation = (arr) => {
+	var notFound = false;
+	for (var i = 0; i < arr[1].length; i++) {
+		var char = arr[1][i];
+		if (arr[0].toLowerCase().indexOf(char.toLowerCase()) === -1) {
+			notFound = true;
+		}
+	}
+	return !notFound;
+}
+
+module.exports.destroyer = (arr, ...args) => {	
+	var newItem = arr.filter((item, index) => {
+		if (args.indexOf(item) === -1) {
+			return item;
+		}
+	});
+	return newItem;
+}
+
+// console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
--- test/basic.spec.js
+++ test/basic.spec.js
@@ -1,8 +1,15 @@
 const mocha = require('mocha');
 const expect = require('chai').expect;
-// const should = require('chai').should;
 const reverseString = require('../app/basic').reverseString;
 const factorize = require('../app/basic').factorize;
+const palindrome = require('../app/basic').palindrome;
+const findLongestWord = require('../app/basic').findLongestWord;
+const titleCase = require('../app/basic').titleCase;
+const confirmEnding = require('../app/basic').confirmEnding;
+const repeatStringNumTimes = require('../app/basic').repeatStringNumTimes;
+const truncateString = require('../app/basic').truncateString;
+const mutation = require('../app/basic').mutation;
+const destroyer = require('../app/basic').destroyer;
 
 describe('Basic Algorithms', () => {
    
@@ -15,7 +22,6 @@ describe('Basic Algorithms', () => {
             expect(reverseString('Hello!')).to.be.equal('!olleH');
         });
     });
-
    
     describe('#factorize', () => {
         it('should be a number', () => {
@@ -28,6 +34,255 @@ describe('Basic Algorithms', () => {
             expect(factorize(5)).to.be.equal(120);
             expect(factorize(10)).to.be.equal(3628800);
             expect(factorize(20)).to.be.equal(2432902008176640000);
+        });
+    });
+    
+    describe('#palindrome', () => {
+        it('should be a boolean', () => {
+            expect(palindrome('eye')).to.be.a('boolean');            
+        });
+
+        it('should be true', () => {
+            expect(palindrome('eye')).to.be.equal(true);            
+        });
+
+        it('should be true', () => {
+            expect(palindrome('_eye')).to.be.equal(true);
+        });
+
+        it('should be true', () => {
+            expect(palindrome('race car')).to.be.equal(true);
+        });
+        
+        it('should be true', () => {
+            expect(palindrome('A man, a plan, a canal. Panama')).to.be.equal(true);
+        });
+       
+        it('should be true', () => {
+            expect(palindrome('never odd or even')).to.be.equal(true);
+        });       
+        
+        it('should be true', () => {
+            expect(palindrome('My age is 0, 0 si ega ym.')).to.be.equal(true);
+        });
+        
+        it('should be true', () => {
+            expect(palindrome('0_0 (: /-\ :) 0-0')).to.be.equal(true);
+        });
+        
+        it('should be false', () => {
+            expect(palindrome('five|\_/|four')).to.be.equal(false);            
+        });
+        
+        it('should be false', () => {
+            expect(palindrome('nope')).to.be.equal(false);            
+        });
+        
+        it('should be false', () => {
+            expect(palindrome('almostomla')).to.be.equal(false);   
+        });
+        
+        it('should be false', () => {
+            expect(palindrome('not a palindrome')).to.be.equal(false);
+        });
+
+        it('should be false', () => {
+            expect(palindrome('1 eye for of 1 eye.')).to.be.equal(false);           
+        });
+    });
+    
+    describe('#findLongestWord', () => {
+        it('should be a number', () => {                       
+            expect(findLongestWord('The quick brown fox jumped over the lazy dog')).to.be.a('number');
+        });
+
+        it('should be equal 6', () => {                       
+            expect(findLongestWord('The quick brown fox jumped over the lazy dog')).to.be.equal(6);
+        });
+
+        it('should be equal to 5', () => {                       
+            expect(findLongestWord('May the force be with you')).to.be.equal(5);
+        });
+
+        it('should be equal to 6', () => {                       
+            expect(findLongestWord('Google do a barrel roll')).to.be.equal(6);
+        });
+
+        it('should be equal to 8', () => {                       
+            expect(findLongestWord('What is the average airspeed velocity of an unladen swallow')).to.be.equal(8);
+        });
+
+        it('should be equal to 19', () => {                       
+            expect(findLongestWord('What if we try a super-long word such as otorhinolaryngology')).to.be.equal(19);
+        });
+    });
+    
+    describe('#titleCase', () => {
+        it('should be a string', () => {
+            expect(titleCase('I\'m a little tea pot')).to.be.a('string');
+        });
+
+        it('should be equal "I\'m A Little Tea Pot"', () => {
+            expect(titleCase('I\'m a little tea pot')).to.be.equal("I'm A Little Tea Pot");
+        });
+
+        it('should be equal to "Short And Stout"', () => {
+            expect(titleCase('sHoRt AnD sToUt')).to.be.equal('Short And Stout');
+        });
+
+        it('should be equal to "Here Is My Handle Here Is My Spout"', () => {                       
+            expect(titleCase('HERE IS MY HANDLE HERE IS MY SPOUT')).to.be.equal('Here Is My Handle Here Is My Spout');
+        });
+    });
+    
+    describe('#confirmEnding', () => {
+        it('should be a boolean', () => {
+            expect(confirmEnding("Bastian", "n")).to.be.a('boolean');
+        });
+        
+        it('should return true', () => {
+            expect(confirmEnding("Bastian", "n")).to.be.equal(true);
+        });
+
+        it('should return false', () => {
+            expect(confirmEnding("Connor", "n")).to.be.equal(false);
+        });
+        
+        it('should return false', () => {
+            expect(confirmEnding("Walking on water and developing software from a specification are easy if both are frozen", "specification")).to.be.equal(false);
+        });
+
+        it('should return true', () => {
+            expect(confirmEnding("Open sesame", "same")).to.be.equal(true);
+        });
+        
+        it('should return false', () => {
+            expect(confirmEnding("If you want to save our world, you must hurry. We dont know how much longer we can withstand the nothing", "mountain")).to.be.equal(false);
+        });
+    });
+    
+    describe('#repeatStringNumTimes', () => {
+        it('should be return string', () => {
+            expect(repeatStringNumTimes("*", 3)).to.be.a("string");
+        });
+
+        it('should be equal to ***', () => {
+            expect(repeatStringNumTimes("*", 3)).to.be.equal("***");
+        });
+        
+        it('should be equal to abcabcabc', () => {
+            expect(repeatStringNumTimes("abc", 3)).to.be.equal("abcabcabc");
+        });
+
+        it('should be equal to abcabcabcabc', () => {
+            expect(repeatStringNumTimes("abc", 4) ).to.be.equal("abcabcabcabc");
+        });
+        
+        it('should be equal to abc', () => {
+            expect(repeatStringNumTimes("abc", 1)).to.be.equal("abc");
+        });
+
+        it('should be eaqual to *******', () => {
+            expect(repeatStringNumTimes("*", 8)).to.be.equal("********");
+        });
+        
+        it('should return empty string', () => {
+            expect(repeatStringNumTimes("abc", -2)).to.be.equal("");
+        });
+    });
+    
+    describe('#truncateString', () => {
+        it('should be return string', () => {
+            expect(truncateString("A-tisket a-tasket A green and yellow basket", 11)).to.be.a("string");
+        });
+
+        it('should be return A-tisket...', () => {
+            expect(truncateString("A-tisket a-tasket A green and yellow basket", 11)).to.be.equal("A-tisket...");
+        });
+        
+        it('should be equal to Peter Piper...', () => {
+            expect(truncateString("Peter Piper picked a peck of pickled peppers", 14)).to.be.equal("Peter Piper...");
+        });
+
+        it('should be equal to A-tisket a-tasket A green and yellow basket', () => {
+            expect(truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length) ).to.be.equal("A-tisket a-tasket A green and yellow basket");
+        });
+        
+        it('should be equal to abc', () => {
+            expect(truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length + 2)).to.be.equal("A-tisket a-tasket A green and yellow basket");
+        });
+
+        it('should be eaqual to *******', () => {
+            expect(truncateString("A-", 1)).to.be.equal("A...");
+        });
+        
+        it('should return empty string', () => {
+            expect(truncateString("Absolutely Longer", 2)).to.be.equal("Ab...");
+        });
+    });
+    
+    
+    describe('#mutation', () => {
+        it('should be return boolean', () => {
+            expect(mutation(["hello", "hey"])).to.be.a('boolean');
+        });
+        
+        it('should return false', () => {
+            expect(mutation(["hello", "hey"])).to.be.equal(false);
+        });
+        
+        it('should return true', () => {
+            expect(mutation(["hello", "Hello"])).to.be.equal(true);
+        });
+        
+        it('should return true', () => {
+            expect(mutation(["zyxwvutsrqponmlkjihgfedcba", "qrstu"])).to.be.equal(true);
+        });
+        
+        it('should return true', () => {
+            expect(mutation(["Mary", "Army"])).to.be.equal(true);
+        });
+        
+        it('should return true', () => {
+            expect(mutation(["Mary", "Aarmy"])).to.be.equal(true);
+        });
+        
+        it('should return true', () => {
+            expect(mutation(["Alien", "line"])).to.be.equal(true);
+        });
+        
+        it('should return true', () => {
+            expect(mutation(["floor", "for"])).to.be.equal(true);
+        });
+        
+        it('should return false', () => {
+            expect(mutation(["hello", "neo"])).to.be.equal(false);
+        });
+       
+        it('should return false', () => {
+            expect(mutation(["voodoo", "no"])).to.be.equal(false);
+        });
+    });
+    
+    describe('#destroyer', () => {
+        it('should be return [1, 1]', () => {
+            expect(destroyer([1, 2, 3, 1, 2, 3], 2, 3)).to.be.equal([1, 1]);
+        });
+        
+        it('should be equal to [1, 5, 1]', () => {
+            expect(destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3)).to.be.equal([1, 5, 1]);
+        });
+        
+        it('should be equal to [1]', () => {
+            expect(destroyer([3, 5, 1, 2, 2], 2, 3, 5)).to.be.equal([1]);
+        });
+        
+        it('should be equal to []', () => {
+            expect(destroyer([2, 3, 2, 3], 2, 3)).to.be.equal([]);
+        });
+        
+        it('should be equal to ["hamburger"]', () => {
+            expect(destroyer(["tree", "hamburger", 53], "tree", 53)).to.be.equal(["hamburger"]);
         });
     });
 });

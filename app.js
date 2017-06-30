b61d6007f77602a440085c9da00d8e539059ad19
Rabii Luena <luenarabii@gmail.com>
2017-06-30 11:22:09 +0300

    Done basic

--- app.js
+++ app.js
@@ -1,22 +1,3 @@
-const reverseString = require('./app/basic').reverseString;
-// const palindrome = require('app/basic').palindrome;
-// const findLongestWord = require('app/basic').findLongestWord;
-// const largestOfFour = require('app/basic').largestOfFour;
-// const confirmEnding = require('app/basic').confirmEnding;
-// const repeatStringNumTimes = require('app/basic').repeatStringNumTimes;
-// const truncateString = require('app/basic').truncateString;
-// const chunkArrayInGroups = require('app/basic').chunkArrayInGroups;
-// const slasher = require('app/basic').slasher;
-// const mutation = require('app/basic').mutation;
-// const destroyer = require('app/basic').destroyer;
-// const getIndexToIns = require('app/basic').getIndexToIns;
-// const rot13 = require('app/basic').rot13;
-
-
-
-
-
-
 /*
     MEDIUM ARTICLES
     1. What I have learned from freeCodeCamp beyond their carriculum.
